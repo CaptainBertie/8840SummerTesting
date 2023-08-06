2e1ffdb6ebc3cbed92a733a88eb04a1b94f3c3be
CaptainBertie <60792964+CaptainBertie@users.noreply.github.com>
2023-08-05 19:08:25 -0700

    Add files via upload

--- MotorMoving/src/main/java/frc/robot/Settings.java
+++ MotorMoving/src/main/java/frc/robot/Settings.java
@@ -1,13 +1,15 @@
 package frc.robot;
 
+import frc.robot.subsystems.Roller;
+
 public class Settings {
     public class General{
         public static final int OPERATR_CONTROLLER_PORT = 0;
     }
     public class Roller {
         public static final int ROLLER_MOTOR_ID = 33;
-        public static final double FAST_OUTTAKE_SPEED = 0.7;
-        public static final double SLOW_OUTTAKE_SPEED = 0.1;
+       // public static final double FAST_OUTTAKE_SPEED = 0.7;
+       // public static final double SLOW_OUTTAKE_SPEED = 0.1;
         public static final double INTAKE_SPEED = -0.7;
         
 }
